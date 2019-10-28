bba4b5f603b63dd9c333be18b025181be25545b6
Dariusz Czajkowski <mtx14@o2.pl>
2019-10-28 20:51:29 +0100

    change code in game.java, all test done successful

--- Game.java
+++ Game.java
@@ -152,31 +152,40 @@ public class Game {
         String result = "None";
         //Student code goes here ...
 
-            for (int i = 0 ; i<3 ; i++) {
+        int checkO=0;
 
-                    if (grid[i][0] == 'x' && grid[i][1]=='x' && grid[i][2]=='x' ||
-                            grid[0][i] == 'x' && grid[1][i]=='x' && grid[2][i]=='x'||
-                    grid[0][0] == 'x' && grid[1][1]=='x' && grid[2][2]=='x' ||
-                    grid[2][0]=='x' && grid[1][1]=='x' && grid[0][2]=='x')
+        for (int i = 0 ; i<3 ; i++) {
+            for (int j=0; j<3 ; j++){
+               if (checkO==4 ) {
+               return result="tie" ;}
+
+                if (grid[i][j]=='o'){
+                    checkO++;
+                }
+
+
+                if (grid[i][0] == 'x' && grid[i][1]=='x' && grid[i][2]=='x' ||
+                        grid[0][i] == 'x' && grid[1][i]=='x' && grid[2][i]=='x'||
+                        grid[0][0] == 'x' && grid[1][1]=='x' && grid[2][2]=='x' ||
+                        grid[2][0]=='x' && grid[1][1]=='x' && grid[0][2]=='x')
                 {
 
 
 
-                    return result = " x wins";
+                    return result = "x wins";
                 }
-                 else if (grid[i][0] == 'o' && grid[i][1]=='o' && grid[i][2]=='o' ||
-                grid[0][i] == 'o' && grid[1][i]=='o' && grid[2][i]=='o'||
-                            grid[0][0] == 'o' && grid[1][1]=='o' && grid[2][2]=='o' ||
-                            grid[2][0]=='o' && grid[1][1]=='o' && grid[0][2]=='o') {
-                    return result = " o wins";
+                else if (grid[i][0] == 'o' && grid[i][1]=='o' && grid[i][2]=='o' ||
+                        grid[0][i] == 'o' && grid[1][i]=='o' && grid[2][i]=='o'||
+                        grid[0][0] == 'o' && grid[1][1]=='o' && grid[2][2]=='o' ||
+                        grid[2][0]=='o' && grid[1][1]=='o' && grid[0][2]=='o') {
+                    return result = "o wins";
 
-                } else if (freeSpots == 0) {
-                    return result = " Tie";
-                }
 
+               }
             }
-                return result;
-    }
+        }
+        return result;
+   }
 
     /**
      * Main function
